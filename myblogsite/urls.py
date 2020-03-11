a4de59b6483408d04e7aa3403007847c9c277160
Michael Osamwonyi <m.osamwonyi@gmail.com>
2020-03-11 14:43:24 +0000

    changed the html for the site

--- myblogsite/urls.py
+++ myblogsite/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from blog.admin import advanced_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('advance-admin/', advanced_site.urls)
+    path('', include('blog.urls')),
 ]
